cfaab9cb5d7892fc6218fc89af1bd8990328e535
eugene-hovorun <oktbvo@gmail.com>
2026-02-10 15:54:50 +0100

    chore: sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint", "@nuxt/ui"],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxtjs/sitemap"],
+
+  site: {
+    url: "https://exthub.dev",
+  },
+
   nitro: {
     preset: "vercel",
     prerender: {
@@ -21,13 +26,4 @@ export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
 
   compatibilityDate: "2025-01-15",
-
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: "never",
-        braceStyle: "1tbs",
-      },
-    },
-  },
 });
